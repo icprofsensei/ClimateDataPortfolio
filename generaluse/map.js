a8d2a7bb1485328d6f99ae27261b6f1511e249fa
Dhylan Patel <139132349+icprofsensei@users.noreply.github.com>
2025-11-29 21:31:30 +0000

    Update map.js

--- generaluse/map.js
+++ generaluse/map.js
@@ -1,5 +1,5 @@
 function jsonfetch(fileloc) {
-    return fetch(`/fileloc`)
+    return fetch(fileloc)
       .then(response => {
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
         return response.json(); // Directly parse JSON
@@ -11,7 +11,7 @@ function jsonfetch(fileloc) {
   }
   
   function arraymaker(fileloc) {
-    return fetch(concat('/', fileloc))
+    return fetch(fileloc)
       .then(response => {
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
         return response.text();
